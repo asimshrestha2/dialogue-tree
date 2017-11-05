7462238cc90794b7468b67d58a23f63308234314
asimshrestha2 <asims178@gmail.com>
2017-11-05 00:39:07 -0400

    Added Lines that will update when you move the DIVs

--- dist/public/js/main.js
+++ dist/public/js/main.js
@@ -36,6 +36,17 @@ function dragElement(elmnt) {
         tpos.y = e.clientY;
         elmnt.style.top = (elmnt.offsetTop - pos.y) + "px";
         elmnt.style.left = (elmnt.offsetLeft - pos.x) + "px";
+        var id = elmnt.getAttribute("data-id");
+        var svgs = document.querySelectorAll("svg[link*=\"" + id + "\"]");
+        console.log(svgs);
+        for (var i = 0; i < svgs.length; i++) {
+            var svg = svgs[i];
+            var link = JSON.parse(svg.getAttribute("link"));
+            var otherID = (link.start == id) ? link.end : link.start;
+            var elmnt1 = document.querySelector(".dialogue[data-id='" + otherID + "']");
+            var d = getDimensionDiff(elmnt1, elmnt);
+            updateLine(d, elmnt1, elmnt, svg);
+        }
     }
     function closeDragElement() {
         document.onmouseup = null;
@@ -56,10 +67,11 @@ function setLinks(elmnt) {
     entry.onclick = function (e) {
         var id = elmnt.getAttribute('data-id');
         console.log(elmnt.offsetTop, elmnt.offsetLeft);
-        if (currentID) {
+        if (currentID && dialogues[currentID].nextDialogue.indexOf(parseInt(id)) < 0) {
             dialogues[currentID].addNextDialogue(parseInt(id));
-            var d = getDimensionDiff(document.querySelector(".dialogue[data-id='" + currentID + "']"), elmnt);
-            var shtml = createLine(d);
+            var elmnt1 = document.querySelector(".dialogue[data-id='" + currentID + "']");
+            var d = getDimensionDiff(elmnt1, elmnt);
+            var shtml = createLine(d, elmnt1, elmnt);
             var projectE = document.getElementById('project');
             projectE.appendChild(shtml);
             currentID = null;
@@ -72,12 +84,34 @@ var makeSVG = function (tag, attrs) {
         el.setAttribute(k, attrs[k]);
     return el;
 };
-function createLine(dimension) {
+var updateLine = function (dimension, elmnt1, elmnt2, elmnt) {
+    elmnt.setAttribute('viewbox', "0 0 " + dimension.height + " " + dimension.width);
+    elmnt.setAttribute('style', "position: absolute; top: " + dimension.top + "; left: " + dimension.left
+        + "; width: " + dimension.width + "px; height: " + dimension.height + "px;");
+    var line = elmnt.querySelector('line');
+    var attrs = {};
+    if (elmnt1.offsetTop < elmnt2.offsetTop) {
+        attrs = (elmnt1.offsetLeft < elmnt2.offsetLeft) ?
+            { x1: "0", y1: "0", x2: dimension.width, y2: dimension.height } :
+            { x1: "0", y1: dimension.height, y2: "0", x2: dimension.width };
+    }
+    else {
+        attrs = (elmnt1.offsetLeft < elmnt2.offsetLeft) ?
+            { x1: "0", y1: dimension.height, y2: "0", x2: dimension.width } :
+            { x1: "0", y1: "0", x2: dimension.width, y2: dimension.height };
+    }
+    for (var k in attrs)
+        line.setAttribute(k, attrs[k]);
+};
+function createLine(dimension, elmnt1, elmnt2) {
     var retE = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     retE.setAttribute('viewbox', "0 0 " + dimension.height + " " + dimension.width);
     retE.setAttribute('style', "position: absolute; top: " + dimension.top + "; left: " + dimension.left
         + "; width: " + dimension.width + "px; height: " + dimension.height + "px;");
-    var line = makeSVG("line", { x1: "0", y1: "0", x2: dimension.width, y2: dimension.height, "stroke-width": "2", stroke: "black" });
+    retE.setAttribute('link', JSON.stringify({ start: elmnt1.getAttribute('data-id'), end: elmnt2.getAttribute('data-id') }));
+    var line = (elmnt1.offsetTop < elmnt2.offsetTop) ?
+        makeSVG("line", { x1: "0", y1: "0", x2: dimension.width, y2: dimension.height, "stroke-width": "2", stroke: "black" }) :
+        makeSVG("line", { x1: "0", y2: "0", x2: dimension.width, y1: dimension.height, "stroke-width": "2", stroke: "black" });
     retE.appendChild(line);
     return retE;
 }
@@ -87,11 +121,23 @@ function getDimensionDiff(elmnt1, elmnt2) {
     var leftMin = Math.min(elmnt1.offsetLeft, elmnt2.offsetLeft);
     var leftMax = Math.max(elmnt1.offsetLeft, elmnt2.offsetLeft);
     var rtn = {
-        top: topMin, left: leftMin,
-        width: leftMax - leftMin, height: topMax - topMin
+        top: (topMin == elmnt1.offsetTop) ? topMin + (elmnt1.offsetHeight / 2) : topMin + (elmnt2.offsetHeight / 2),
+        left: (leftMin == elmnt1.offsetLeft) ? leftMin + (elmnt1.offsetWidth) : leftMin + (elmnt2.offsetWidth),
+        width: (leftMin == elmnt1.offsetLeft) ? leftMax - leftMin - (elmnt1.offsetWidth) : leftMax - leftMin - (elmnt2.offsetWidth),
+        height: (topMin == elmnt1.offsetTop) ? topMax - topMin - (elmnt1.offsetHeight / 2) + (elmnt2.offsetHeight / 2) :
+            topMax - topMin - (elmnt2.offsetHeight / 2) + (elmnt1.offsetHeight / 2)
     };
     console.log(rtn);
     return rtn;
+}
+function exportData(name, data) {
+    var a = document.createElement('a');
+    a.setAttribute('download', name);
+    a.setAttribute('href', 'data:application/json,' + encodeURIComponent(JSON.stringify(data)));
+    a.setAttribute('target', '_blank');
+    a.innerHTML = "Link";
+    var projectE = document.getElementById('project');
+    projectE.appendChild(a);
 }
 document.addEventListener("DOMContentLoaded", function () {
     var count = 0;
--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -48,6 +48,18 @@ function dragElement(elmnt) {
         // set the element's new position:
         elmnt.style.top = (elmnt.offsetTop - pos.y) + "px";
         elmnt.style.left = (elmnt.offsetLeft - pos.x) + "px";
+        
+        var id = elmnt.getAttribute("data-id");
+        var svgs = document.querySelectorAll("svg[link*=\"" + id + "\"]");
+        console.log(svgs);
+        for (var i = 0; i < svgs.length; i++) {
+            var svg = svgs[i];
+            var link = JSON.parse(svg.getAttribute("link"));
+            var otherID = (link.start == id) ? link.end : link.start;
+            var elmnt1 = document.querySelector(".dialogue[data-id='" + otherID + "']") as HTMLDivElement; 
+            var d = getDimensionDiff(elmnt1, elmnt);
+            updateLine(d, elmnt1, elmnt, svg as SVGElement);
+        }
     }
   
     function closeDragElement() {
@@ -73,10 +85,11 @@ function setLinks(elmnt: HTMLDivElement){
     entry.onclick = (e) => {
         var id = elmnt.getAttribute('data-id');
         console.log(elmnt.offsetTop, elmnt.offsetLeft);
-        if(currentID){
+        if(currentID && dialogues[currentID].nextDialogue.indexOf(parseInt(id)) < 0){
             dialogues[currentID].addNextDialogue(parseInt(id));
-            var d = getDimensionDiff(document.querySelector(".dialogue[data-id='" + currentID + "']") as HTMLDivElement, elmnt);
-            var shtml = createLine(d);
+            var elmnt1 = document.querySelector(".dialogue[data-id='" + currentID + "']") as HTMLDivElement; 
+            var d = getDimensionDiff(elmnt1, elmnt);
+            var shtml = createLine(d, elmnt1, elmnt);
             var projectE = document.getElementById('project');
             projectE.appendChild(shtml);
             currentID = null
@@ -98,12 +111,36 @@ var makeSVG = (tag: string, attrs: {}) => {
     return el;
 }
 
-function createLine(dimension: Dimension){
+var updateLine = (dimension: Dimension, elmnt1: HTMLDivElement, elmnt2: HTMLDivElement, elmnt: SVGElement) => {
+    elmnt.setAttribute('viewbox', "0 0 " + dimension.height + " " + dimension.width);
+    elmnt.setAttribute('style', "position: absolute; top: " + dimension.top + "; left: " + dimension.left
+         + "; width: " + dimension.width + "px; height: " + dimension.height + "px;");
+    
+    var line = elmnt.querySelector('line') as SVGElement
+    var attrs = {};
+    
+    if(elmnt1.offsetTop < elmnt2.offsetTop){
+        attrs = (elmnt1.offsetLeft < elmnt2.offsetLeft) ?
+            {x1: "0", y1: "0", x2: dimension.width, y2: dimension.height} :
+            {x1: "0", y1: dimension.height, y2: "0", x2: dimension.width}
+    } else {
+        attrs = (elmnt1.offsetLeft < elmnt2.offsetLeft) ?
+            {x1: "0", y1: dimension.height, y2: "0", x2: dimension.width} :
+            {x1: "0", y1: "0", x2: dimension.width, y2: dimension.height}
+    }
+    for (var k in attrs)
+        line.setAttribute(k, attrs[k]);
+}
+
+function createLine(dimension: Dimension, elmnt1: HTMLDivElement, elmnt2: HTMLDivElement){
     var retE = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     retE.setAttribute('viewbox', "0 0 " + dimension.height + " " + dimension.width);
     retE.setAttribute('style', "position: absolute; top: " + dimension.top + "; left: " + dimension.left
          + "; width: " + dimension.width + "px; height: " + dimension.height + "px;");
-    var line = makeSVG("line", {x1: "0", y1: "0", x2: dimension.width, y2: dimension.height, "stroke-width": "2", stroke:"black" })
+    retE.setAttribute('link', JSON.stringify({start: elmnt1.getAttribute('data-id'), end: elmnt2.getAttribute('data-id')}));
+    var line = (elmnt1.offsetTop < elmnt2.offsetTop)?
+        makeSVG("line", {x1: "0", y1: "0", x2: dimension.width, y2: dimension.height, "stroke-width": "2", stroke:"black" }) :
+        makeSVG("line", {x1: "0", y2: "0", x2: dimension.width, y1: dimension.height, "stroke-width": "2", stroke:"black" })
     retE.appendChild(line);
     return retE;
 }
@@ -113,12 +150,26 @@ function getDimensionDiff(elmnt1: HTMLDivElement, elmnt2: HTMLDivElement){
     var topMax = Math.max(elmnt1.offsetTop, elmnt2.offsetTop)
     var leftMin = Math.min(elmnt1.offsetLeft, elmnt2.offsetLeft)
     var leftMax = Math.max(elmnt1.offsetLeft, elmnt2.offsetLeft)
+
     var rtn: Dimension = {
-        top: topMin, left: leftMin,
-        width: leftMax - leftMin, height: topMax - topMin
+        top: (topMin == elmnt1.offsetTop)? topMin + (elmnt1.offsetHeight/2) : topMin + (elmnt2.offsetHeight/2), 
+        left: (leftMin == elmnt1.offsetLeft)? leftMin + (elmnt1.offsetWidth) : leftMin + (elmnt2.offsetWidth),
+        width: (leftMin == elmnt1.offsetLeft)? leftMax - leftMin - (elmnt1.offsetWidth) : leftMax - leftMin - (elmnt2.offsetWidth),
+        height: (topMin == elmnt1.offsetTop)? topMax - topMin - (elmnt1.offsetHeight/2) + (elmnt2.offsetHeight/2):
+        topMax - topMin - (elmnt2.offsetHeight/2) + (elmnt1.offsetHeight/2)
     }
     console.log(rtn);
     return rtn;
+}
+
+function exportData(name: string, data: Dialogue[]){
+    var a = document.createElement('a');
+    a.setAttribute('download', name);
+    a.setAttribute('href', 'data:application/json,' + encodeURIComponent(JSON.stringify(data)));
+    a.setAttribute('target', '_blank');
+    a.innerHTML = "Link";
+    var projectE = document.getElementById('project');
+    projectE.appendChild(a);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
